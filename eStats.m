000dfb61e6a4183187e8fcb80d6354108c555eac
esmith36 <39316621+esmith36@users.noreply.github.com>
2018-06-10 23:34:48 -0400

    6/10

---
+++ eStats.m
@@ -0,0 +1,26 @@
+function [RMSE,Rsquared]= eStats(real, predict) 
+% eStats(real,predict)
+% Calculates the RMSE value and R squared value 
+
+N = length(real); 
+diff = zeros(N,1);
+for i = 1:N
+    diff(i) = real(i) - predict(i); 
+end 
+%% RMSE
+squared_diff = diff.^2; 
+sum_squared_diff = sum(squared_diff);
+MSE = sum_squared_diff / N; 
+
+RMSE = sqrt(MSE); 
+
+%% R squared 
+average = mean(real); 
+variance = zeros(N,1); 
+for i = 1:N
+    variance(i) = real(i) - average; 
+end 
+squared_variance = variance.^2; 
+SS_tot = sum(squared_variance); 
+SS_res = sum_squared_diff; 
+Rsquared = 1 - SS_res/SS_tot; 
--- spec10a.m
+++ spec10a.m
@@ -15,8 +15,34 @@ a = 2.5744; nhex = 5; vsim = linspace(-0.4, 0.5, 451)';
 sf =10; vp = khex(nhex, sf*a,1); vspec=[0,sf*a/sqrt(3); sf*a/2,0]; 
 simh10=zeros(size(vsim,1),size(vspec,1));
 for ni=1:size(vspec,1)
-    simh10(:,ni) = kspec(vp, vspec(ni,:), vsim,(-.11630681+0.20520943*sqrt(-1)),disp); % change for mine to compare plots
+    simh10(:,ni) = kspec(vp, vspec(ni,:), vsim,(-.09230312+0.18916686*sqrt(-1)),disp); % change for mine to compare plots
 end
 
 figure; plot(v,h10tr,vsim*1000,simh10(:,1));
 figure; plot(v,h10br,vsim*1000,simh10(:,2));
+
+%% Evaluating Plots 
+h10tr = h10tr(101:end); 
+j = 1; 
+h10tr_new = zeros(451,1);
+for i = 1:2:901
+    h10tr_new(j) = h10tr(i); 
+    j = j + 1; 
+end
+h10tr_new = h10tr_new'; 
+
+
+h10br_new = zeros(451,1);
+h10br = h10br(101:end); 
+j = 1; 
+for i = 1:2:901
+    h10br_new(j) = h10br(i); 
+    j = j + 1; 
+end
+h10br_new = h10br_new'; 
+
+simh10tr = simh10(:,1);
+simh10br = simh10(:,2); 
+
+[RMSE, Rsquared] = eStats(h10tr_new, simh10tr); 
+[RMSE2, Rsquared2] = eStats(h10br_new, simh10br); 
