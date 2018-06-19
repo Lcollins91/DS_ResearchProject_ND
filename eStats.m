c748e2c345454667bfabde40ae327cff5907d2f4
esmith36 <39316621+esmith36@users.noreply.github.com>
2018-06-18 20:05:28 -0400

    6/18

--- eStats.m
+++ eStats.m
@@ -1,11 +1,11 @@
-function [RMSE,Rsquared, Residuals, Av_residual]= eStats(real, predict) 
+function [RMSE, Residuals, Av_residual]= eStats(real, predict) 
 % eStats(real,predict)
 % Calculates the RMSE value and R squared value 
 
 N = length(real); 
 diff = zeros(N,1);
 for i = 1:N
-    diff(i) = real(i) - predict(i); 
+    diff(i) = abs(real(i) - predict(i)); 
 end 
 Residuals = diff; 
 Av_residual = sum(diff)/N; 
@@ -16,13 +16,3 @@ MSE = sum_squared_diff / N;
 
 RMSE = sqrt(MSE); 
 
-%% R squared 
-average = mean(real); 
-variance = zeros(N,1); 
-for i = 1:N
-    variance(i) = real(i) - average; 
-end 
-squared_variance = variance.^2; 
-SS_tot = sum(squared_variance); 
-SS_res = sum_squared_diff; 
-Rsquared = 1 - SS_res/SS_tot; 
